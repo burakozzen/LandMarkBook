b61168d6ac024a7c4e5b035b50c775333eea7364
Burak ÖZEN <burakozen91@gmail.com>
2022-03-12 21:44:56 +0300

    Initial Commit

---
+++ LandMarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandMarkBook
+//
+//  Created by Burak ÖZEN on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
